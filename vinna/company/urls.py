957124c2ff51340f0a408d2785e7e8a266691a1f
Jc O. Din <ozknightwalker62@gmail.com>
2015-07-13 10:46:06 +0800

    Added Company View

--- vinna/company/urls.py
+++ vinna/company/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     url(r'^new$', CreateCompanyView.as_view(), name='create'),
+    url(r'^owned$', OwnedCompanyView.as_view(), name='owned'),
 ]
--- vinna/company/views.py
+++ vinna/company/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.views import generic
 from .models import Company
 from .forms import CompanyCreationForm
+from vinna.settings import MEDIA_URL
 # Create your views here.
 
 
@@ -36,3 +37,15 @@ class CreateCompanyView(generic.CreateView):
             messages.add_message(
                 request, messages.ERROR, 'Something Went Wrong!')
             return self.form_invalid(form)
+
+
+class OwnedCompanyView(generic.TemplateView):
+    model = Company
+    template_name = 'Company_Created.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(OwnedCompanyView, self).get_context_data(**kwargs)
+        context['page_title'] = "Your Company"
+        context['company'] = Company.objects.get(owner=self.request.user)
+        context['media'] = MEDIA_URL
+        return context
